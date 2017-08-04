ac3970e047f127481c99a848514e48fa8e79b15e
Darnell Williams <williams.darnell56@yahoo.com>
2017-08-04 13:15:55 -0400

    Adds Location Validation

---
+++ HyddwnLauncher/Util/Validation/LocationValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HyddwnLauncher.Util.Validation
+{
+    public class LocationValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var str = value as string;
+
+            if (string.IsNullOrWhiteSpace(str))
+                return new ValidationResult(false, "Location is required!");
+
+            if (!File.Exists(str))
+                return new ValidationResult(false, "File does not exist!");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
