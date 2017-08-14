0f6674c871643b5a043c7f747861d867a4e6dd47
Darnell Williams <williams.darnell56@yahoo.com>
2017-08-13 21:33:47 -0400

    Adds PluginBase

---
+++ HyddwnLauncher.Extensibility/PluginBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HyddwnLauncher.Extensibility.Interfaces;
+using HyddwnLauncher.Extensibility.Primitives;
+
+namespace HyddwnLauncher.Extensibility
+{
+    public abstract class PluginBase : IPlugin
+    {
+        public string Name { get; }
+
+        public virtual void Initialize(PluginContext pluginContext, IClientProfile activeClientProfile, IServerProfile activeServerProfile)
+        {
+            
+        }
+
+        public virtual Guid GetGuid()
+        {
+            return Guid.NewGuid();
+        }
+
+        public virtual void Shutdown()
+        {
+            
+        }
+
+        public virtual void ClientProfileChanged(IClientProfile clientProfile)
+        {
+            
+        }
+
+        public virtual void ServerProfileChanged(IServerProfile serverProfile)
+        {
+            
+        }
+
+        public virtual void PreLaunch()
+        {
+            
+        }
+
+        public virtual void PostLaunch()
+        {
+            
+        }
+    }
+}
