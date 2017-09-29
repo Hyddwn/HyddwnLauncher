a2532ecbd0ce8d6ee172d79e1790d659a6918031
Darnell Williams <williams.darnell56@yahoo.com>
2017-09-29 12:36:08 -0400

    Tabify Credentials Storage

--- HyddwnLauncher/Core/CredentialsStorage.cs
+++ HyddwnLauncher/Core/CredentialsStorage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -8,132 +7,132 @@ using Newtonsoft.Json;
 
 namespace HyddwnLauncher.Core
 {
-    internal class CredentialsStorage
-    {
-        public ObservableDictionary<string, CredentialsObject> CredntialsObjects { get; }
+	internal class CredentialsStorage
+	{
+		public ObservableDictionary<string, CredentialsObject> CredntialsObjects { get; }
 
-        public readonly string _credentialsJson = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Hyddwn Launcher\\credentials.json";
+		public readonly string _credentialsJson = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Hyddwn Launcher\\credentials.json";
 
-        public static readonly CredentialsStorage Instance = new CredentialsStorage();
+		public static readonly CredentialsStorage Instance = new CredentialsStorage();
 
-        public CredentialsStorage()
-        {
-            if (!Directory.Exists(Path.GetDirectoryName(_credentialsJson)))
-                Directory.CreateDirectory(Path.GetDirectoryName(_credentialsJson));
+		public CredentialsStorage()
+		{
+			if (!Directory.Exists(Path.GetDirectoryName(_credentialsJson)))
+				Directory.CreateDirectory(Path.GetDirectoryName(_credentialsJson));
 
-            CredntialsObjects = Load();
-        }
+			CredntialsObjects = Load();
+		}
 
-        private ObservableDictionary<string, CredentialsObject> Load()
-        {
-            try
-            {
-                var fs = new FileStream(_credentialsJson, FileMode.Open);
-                var sr = new StreamReader(fs);
+		private ObservableDictionary<string, CredentialsObject> Load()
+		{
+			try
+			{
+				var fs = new FileStream(_credentialsJson, FileMode.Open);
+				var sr = new StreamReader(fs);
 
-                var result = JsonConvert.DeserializeObject<ObservableDictionary<string, CredentialsObject>>(sr.ReadToEnd());
+				var result = JsonConvert.DeserializeObject<ObservableDictionary<string, CredentialsObject>>(sr.ReadToEnd());
 
-                sr.Dispose();
-                fs.Dispose();
+				sr.Dispose();
+				fs.Dispose();
 
-                return result;
-            }
-            catch (Exception)
-            {
-                return new ObservableDictionary<string, CredentialsObject>();
-            }
-        }
+				return result;
+			}
+			catch (Exception)
+			{
+				return new ObservableDictionary<string, CredentialsObject>();
+			}
+		}
 
-        public void Save()
-        {
-            try
-            {
-                var json = JsonConvert.SerializeObject(CredntialsObjects, Formatting.Indented);
-                if (File.Exists(_credentialsJson))
-                    File.Delete(_credentialsJson);
+		public void Save()
+		{
+			try
+			{
+				var json = JsonConvert.SerializeObject(CredntialsObjects, Formatting.Indented);
+				if (File.Exists(_credentialsJson))
+					File.Delete(_credentialsJson);
 
-                File.WriteAllText(_credentialsJson, json);
-            }
-            catch (Exception ex)
-            {
-                Log.Exception(ex, "Unable to save server credential data");
-                MessageBox.Show("Unable to save credential data.\r\n\r\n" + ex.Message, "Error");
-            }
-        }
+				File.WriteAllText(_credentialsJson, json);
+			}
+			catch (Exception ex)
+			{
+				Log.Exception(ex, "Unable to save server credential data");
+				MessageBox.Show("Unable to save credential data.\r\n\r\n" + ex.Message, "Error");
+			}
+		}
 
-        public void Reset()
-        {
-            CredntialsObjects.Clear();
-            Save();
-        }
+		public void Reset()
+		{
+			CredntialsObjects.Clear();
+			Save();
+		}
 
-        public void Add(string guid, string username, string password)
-        {
-            var credentials = GetCredentialsForProfile(guid);
+		public void Add(string guid, string username, string password)
+		{
+			var credentials = GetCredentialsForProfile(guid);
 
-            if (credentials != null)
-            {
-                credentials = new CredentialsObject(username, password);
-                CredntialsObjects[guid] = credentials;
-            }
-            else
-            {
-                CredntialsObjects.Add(guid, new CredentialsObject(username, password));
-            }
+			if (credentials != null)
+			{
+				credentials = new CredentialsObject(username, password);
+				CredntialsObjects[guid] = credentials;
+			}
+			else
+			{
+				CredntialsObjects.Add(guid, new CredentialsObject(username, password));
+			}
 
-            Save();
-        }
+			Save();
+		}
 
-        public void Update(string guid, string username, string password)
-        {
-            var credentials = GetCredentialsForProfile(guid);
-            if (credentials == null) return;
+		public void Update(string guid, string username, string password)
+		{
+			var credentials = GetCredentialsForProfile(guid);
+			if (credentials == null) return;
 
-            credentials = new CredentialsObject(username, password);
+			credentials = new CredentialsObject(username, password);
 
-            CredntialsObjects[guid] = credentials;
-            Save();
-        }
+			CredntialsObjects[guid] = credentials;
+			Save();
+		}
 
-        public void Remove(string guid)
-        {
-            try
-            {
-                CredntialsObjects.Remove(guid);
-            }
-            catch { }
-            Save();
-        }
+		public void Remove(string guid)
+		{
+			try
+			{
+				CredntialsObjects.Remove(guid);
+			}
+			catch { }
+			Save();
+		}
 
-        public CredentialsObject GetCredentialsForProfile(string guid)
-        {
-            CredntialsObjects.TryGetValue(guid, out var credentials);
+		public CredentialsObject GetCredentialsForProfile(string guid)
+		{
+			CredntialsObjects.TryGetValue(guid, out var credentials);
 
-            return credentials;
-        }
-    }
+			return credentials;
+		}
+	}
 
-    internal class CredentialsObject
-    {
-        private string _username;
-        private string _password;
+	internal class CredentialsObject
+	{
+		private string _username;
+		private string _password;
 
-        public CredentialsObject(string username, string password)
-        {
-            Username = username;
-            Password = password;
-        }
+		public CredentialsObject(string username, string password)
+		{
+			Username = username;
+			Password = password;
+		}
 
-        public string Username
-        {
-            get => Encoding.UTF8.GetString(Convert.FromBase64String(_username));
-            private set => _username = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
-        }
+		public string Username
+		{
+			get => Encoding.UTF8.GetString(Convert.FromBase64String(_username));
+			private set => _username = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+		}
 
-        public string Password
-        {
-            get => Encoding.UTF8.GetString(Convert.FromBase64String(_password));
-            private set => _password = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
-        }
-    }
+		public string Password
+		{
+			get => Encoding.UTF8.GetString(Convert.FromBase64String(_password));
+			private set => _password = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+		}
+	}
 }
