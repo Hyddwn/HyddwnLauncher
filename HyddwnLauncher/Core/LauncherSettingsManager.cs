d4ebe37d35ede54fdeaeabc789cf946500b68939
Darnell Williams <williams.darnell56@yahoo.com>
2017-10-14 23:21:38 -0400

    Attempt to better handle null reference

--- HyddwnLauncher/Core/LauncherSettingsManager.cs
+++ HyddwnLauncher/Core/LauncherSettingsManager.cs
@@ -16,12 +16,35 @@ namespace HyddwnLauncher.Core
 
 		public LauncherSettingsManager()
 		{
-			if (!Directory.Exists(Path.GetDirectoryName(_configurationJson)))
-				Directory.CreateDirectory(Path.GetDirectoryName(_configurationJson));
+		    try
+		    {
+		        if (!Directory.Exists(Path.GetDirectoryName(_configurationJson)))
+		            Directory.CreateDirectory(Path.GetDirectoryName(_configurationJson) ?? throw new ApplicationException("An error occured when attempting to load the configuration data: Path is Null!!!"));
 
-			SettingsManager = new SettingsManager(_configurationJson);
-			LauncherSettings = LoadLauncherSettings();
-			LauncherSettings.SaveOnChanged += SaveOnChanged;
+		        SettingsManager = new SettingsManager(_configurationJson);
+		        LauncherSettings = LoadLauncherSettings();
+		        LauncherSettings.SaveOnChanged += SaveOnChanged;
+            }
+		    catch (ApplicationException e)
+		    {
+		        try
+		        {
+                    Log.Debug(e.Message);
+                    Log.Debug(e.StackTrace);
+
+		            File.Delete(_configurationJson);
+		            SettingsManager = new SettingsManager(_configurationJson);
+		            LauncherSettings = LoadLauncherSettings();
+		            LauncherSettings.SaveOnChanged += SaveOnChanged;
+
+                    Log.Error("An error occured with the configuration file that required it to be reset.");
+		        }
+		        catch (Exception ex)
+		        {
+		            Log.Exception(ex, $"While attempting to recover from corrupt or malformed config: {ex.Message}");
+		            throw;
+		        }
+		    }
 		}
 
 		private void SaveOnChanged(string propertyName)
