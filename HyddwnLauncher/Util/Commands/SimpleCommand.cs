b34f3a612ab322faea6779b75d6cc393a06c78e6
Darnell Williams <williams.darnell56@yahoo.com>
2017-10-24 20:36:06 -0400

    Adds SimpleCommand
    * Didn't need it yet, but as the launcher begins to go through refactoring and I slowly push towards MVVM this is here already.

---
+++ HyddwnLauncher/Util/Commands/SimpleCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HyddwnLauncher.Util.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+        public Predicate<object> CanExecuteDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteDelegate == null || CanExecuteDelegate(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteDelegate?.Invoke(parameter);
+        }
+    }
+}
