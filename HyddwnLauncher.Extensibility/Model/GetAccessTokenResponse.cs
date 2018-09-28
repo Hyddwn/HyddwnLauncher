727594c27bb3ed66760ed98ba2ca6ed766fed4a4
Darnell Williams <williams.darnell56@yahoo.com>
2018-09-28 10:12:03 -0400

    Create Model for Access Token Endpoint response

---
+++ HyddwnLauncher.Extensibility/Model/GetAccessTokenResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyddwnLauncher.Extensibility.Model
+{
+    /// <summary>
+    /// Object modeling the response from Nexon
+    /// </summary>
+    public class GetAccessTokenResponse
+    {
+        [JsonProperty("code")]
+        public string Code { get; set;  }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
