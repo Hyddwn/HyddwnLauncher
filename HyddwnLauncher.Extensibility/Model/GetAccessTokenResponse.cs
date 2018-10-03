24e57592f7d2c8a549b85b38042fb204f628cbe0
Darnell Williams <williams.darnell56@yahoo.com>
2018-10-02 20:19:30 -0400

    Updates GetAccessTokenResponse with documents and change to struct

--- HyddwnLauncher.Extensibility/Model/GetAccessTokenResponse.cs
+++ HyddwnLauncher.Extensibility/Model/GetAccessTokenResponse.cs
@@ -10,15 +10,29 @@ namespace HyddwnLauncher.Extensibility.Model
     /// <summary>
     /// Object modeling the response from Nexon
     /// </summary>
-    public class GetAccessTokenResponse
+    public struct GetAccessTokenResponse
     {
+        /// <summary>
+        /// The response code
+        /// </summary>
         [JsonProperty("code")]
         public string Code { get; set;  }
 
+        /// <summary>
+        /// The description (useless to us)
+        /// </summary>
         [JsonProperty("description")]
         public string Description { get; set; }
 
+        /// <summary>
+        /// The message
+        /// </summary>
         [JsonProperty("message")]
         public string Message { get; set; }
+
+        /// <summary>
+        /// If login was successful
+        /// </summary>
+        public bool Success { get; set; }
     }
 }
