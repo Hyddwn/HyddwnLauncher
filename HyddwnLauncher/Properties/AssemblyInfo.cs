431fe000567b5663454f2213b6e31a57ae707646
Darnell Williams <williams.darnell56@yahoo.com>
2019-07-18 00:20:16 -0400

    Bump Version

--- HyddwnLauncher/Properties/AssemblyInfo.cs
+++ HyddwnLauncher/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.11.18.25")]
-[assembly: AssemblyFileVersion("0.11.18.25")]
+[assembly: AssemblyVersion("0.11.18.26")]
+[assembly: AssemblyFileVersion("0.11.18.26")]
