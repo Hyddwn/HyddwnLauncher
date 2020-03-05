7ce8938c2ef15423393d607ab3c27514452167c6
Darnell Williams <williams.darnell56@yahoo.com>
2020-03-05 09:01:52 -0500

    Adds `GetMaintenanceStatusResponse`

---
+++ HyddwnLauncher.Extensibility/Model/GetMaintenanceStatusResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyddwnLauncher.Extensibility.Model
+{
+    public struct GetMaintenanceStatusResponse
+    {
+        [JsonProperty(PropertyName = "maintenanceNo")]
+        public int MaintenanceNumber { get; }
+
+        [JsonProperty(PropertyName = "maintenanceMode")]
+        public string MaintenanceMode { get; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; }
+    }
+}
