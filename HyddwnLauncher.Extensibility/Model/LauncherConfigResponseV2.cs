8b0487c6454276964ba3c6ede23fd0a99b88e3d8
Darnell Williams <williams.darnell56@yahoo.com>
2020-03-05 09:16:24 -0500

    Updates to match with new API models

---
+++ HyddwnLauncher.Extensibility/Model/LauncherConfigResponseV2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyddwnLauncher.Extensibility.Model
+{
+    /// <summary>
+    /// The object representing the response received when attempting to get launcher configuration data.
+    /// </summary>
+    public class LauncherConfigResponseV2
+    {
+        [JsonProperty("required_uac")]
+        public bool RequireUAC { get; set; }
+
+        [JsonProperty("required_installation_software")]
+        public List<string> PredefinedInstallScripts { get; set; }
+
+        [JsonProperty("working_directory")]
+        public string WorkingDirectory { get; set; }
+
+        [JsonProperty("supported_os")]
+        public SupportedOsV2 SupportedOs { get; set; }
+
+        [JsonProperty("directory_name")]
+        public string DirectoryName { get; set; }
+
+        [JsonProperty("executable_path_bit64")]
+        public string ClientPath64 { get; set; }
+
+        [JsonProperty("patch")]
+        public bool IsPatchAvailable { get; set; }
+
+        [JsonProperty("support_automatic_update")]
+        public bool AutoUpdateSupported { get; set; }
+
+        [JsonProperty("public")]
+        public bool IsPublic { get; set; }
+
+        [JsonProperty("required_disk_space")]
+        public string RequiredDiskSpace { get; set; }
+
+        [JsonProperty("product_id")]
+        public int ProductId { get; set; }
+
+        [JsonProperty("parameter")]
+        public List<string> Arguments { get; set; }
+
+        [JsonProperty("supported_system_type")]
+        public SupportedArch SupportedArch { get; set; }
+
+        [JsonProperty("executable_path")]
+        public string ClientPath { get; set; }
+
+        [JsonProperty("tracking_id")]
+        public object TrackingId { get; set; }
+
+        [JsonProperty("short_cut_name")]
+        public string ShortCutName { get; set; }
+    }
+}
---
+++ HyddwnLauncher.Extensibility/Model/SupportedOsV2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HyddwnLauncher.Extensibility.Model
+{
+    public class SupportedOsV2
+    {
+        [JsonProperty("windows_vista")]
+        public bool WindowsVista { get; set; }
+
+        [JsonProperty("windows_7")]
+        public bool Windows7 { get; set; }
+
+        [JsonProperty("windows_8")]
+        public bool Windows8 { get; set; }
+
+        [JsonProperty("windows_10")]
+        public bool Windows10 { get; set; }
+    }
+}
