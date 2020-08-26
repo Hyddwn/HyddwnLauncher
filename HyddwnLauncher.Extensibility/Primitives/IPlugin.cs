c61adf3f52e9d0f36b07ede31c12dcd586734d85
Darnell Williams <williams.darnell56@yahoo.com>
2020-08-26 09:20:26 -0400

    Adds Documentation for `IPlugin`

--- HyddwnLauncher.Extensibility/Primitives/IPlugin.cs
+++ HyddwnLauncher.Extensibility/Primitives/IPlugin.cs
@@ -6,10 +6,17 @@ namespace HyddwnLauncher.Extensibility.Primitives
 {
     public interface IPlugin
     {
-        // The name of this plugin
+        /// <summary>
+        ///     The name of the plugin
+        /// </summary>
         string Name { get; }
 
-        // The void Main for this plugin
+        /// <summary>
+        ///     The entry point for the plugin
+        /// </summary>
+        /// <param name="pluginContext"></param>
+        /// <param name="activeClientProfile"></param>
+        /// <param name="activeServerProfile"></param>
         void Initialize(PluginContext pluginContext, IClientProfile activeClientProfile,
             IServerProfile activeServerProfile);
 
@@ -24,18 +31,43 @@ namespace HyddwnLauncher.Extensibility.Primitives
         /// </summary>
         void Shutdown();
 
+        /// <summary>
+        ///     Handle when the client profile changes
+        /// </summary>
+        /// <param name="clientProfile"></param>
         void ClientProfileChanged(IClientProfile clientProfile);
 
+        /// <summary>
+        ///     Handle when the server profile changes
+        /// </summary>
+        /// <param name="serverProfile"></param>
         void ServerProfileChanged(IServerProfile serverProfile);
 
+        /// <summary>
+        ///     Handle actions that should be taken before launching the client
+        /// </summary>
         void PreLaunch();
 
+        /// <summary>
+        ///     Handle actions that should be taken after launching the client
+        /// </summary>
         void PostLaunch();
 
+        /// <summary>
+        ///     Called when the launcher it loading to populate a tab in the Launcher
+        /// </summary>
+        /// <returns></returns>
         UserControl GetPluginUi();
 
+        /// <summary>
+        ///     Actions to take before the patcher has started running 
+        /// </summary>
         void PatchBegin();
 
+        /// <summary>
+        ///     Actions to take after the patcher has stopped
+        /// </summary>
+        // TODO: Pass the result back to a plugin?
         void PatchEnd();
     }
 }
