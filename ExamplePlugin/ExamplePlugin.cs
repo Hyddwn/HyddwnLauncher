d93111198df03c51062b996cd21466e479085727
Darnell Williams <williams.darnell56@yahoo.com>
2020-08-26 09:19:54 -0400

    Beginnings of a plugin scaffold

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Composition;
+using HyddwnLauncher.Extensibility;
+using HyddwnLauncher.Extensibility.Interfaces;
+using HyddwnLauncher.Extensibility.Primitives;
+
+namespace ExamplePlugin
+{
+    [Export(typeof(IPlugin))]
+    public class ExamplePlugin : PluginBase
+    {
+        public override void Initialize(PluginContext pluginContext, IClientProfile activeClientProfile, IServerProfile activeServerProfile)
+        {
+            // Set the name of the plugin
+            Name = "Example Plugin";
+
+            // Call the base function (sets needed properties for you)
+            base.Initialize(pluginContext, activeClientProfile, activeServerProfile);
+        }
+    }
+}
