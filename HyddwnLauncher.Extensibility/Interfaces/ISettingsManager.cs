08cb8693e3024eac5b12e6669d461f4cc3fad0a9
Darnell Williams <williams.darnell56@yahoo.com>
2020-09-01 08:03:55 -0400

    Adds documentation from ISettingsManager

--- HyddwnLauncher.Extensibility/Interfaces/ISettingsManager.cs
+++ HyddwnLauncher.Extensibility/Interfaces/ISettingsManager.cs
@@ -2,15 +2,65 @@
 
 namespace HyddwnLauncher.Extensibility.Interfaces
 {
+	/// <summary>
+	///		Represents an object capable of managing settings
+	/// </summary>
 	public interface ISettingsManager
 	{
+		/// <summary>
+		///		Loads settings deserialized as the specified type
+		/// </summary>
+		/// <typeparam name="T">The type that the object should be deserialized as</typeparam>
+		/// <returns></returns>
 		T Load<T>() where T : class, new();
+
+		/// <summary>
+		///		Loads a section of the settings deserialized as the specified type
+		/// </summary>
+		/// <typeparam name="T">The type that the object should be deserialized as</typeparam>
+		/// <returns></returns>
 		T LoadSection<T>() where T : class, new();
+
+		/// <summary>
+		///		Loads settings
+		/// </summary>
+		/// <param name="type">The type that the object should be deserialized as</param>
+		/// <returns></returns>
 		object Load(Type type);
+
+		/// <summary>
+		///		Loads a section of the settings
+		/// </summary>
+		/// <param name="type">The type that the object should be deserialized as</param>
+		/// <returns></returns>
 		object LoadSection(Type type);
+
+		/// <summary>
+		///		Saves an object serialized as json
+		/// </summary>
+		/// <typeparam name="T">The type that the object should be serialized as</typeparam>
+		/// <param name="typeObject">An instance of the type to serialize</param>
 		void Save<T>(T typeObject) where T : class, new();
+
+		/// <summary>
+		///		Saves an object serialized as json as a section
+		/// </summary>
+		/// <typeparam name="T">The type that the object should be serialized as</typeparam>
+		/// <param name="typeObject"></param>
 		void SaveSection<T>(T typeObject) where T : class, new();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="value"></param>
 		void Save(Type type, object value);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="value"></param>
 		void SaveSection(Type type, object value);
 	}
 }
