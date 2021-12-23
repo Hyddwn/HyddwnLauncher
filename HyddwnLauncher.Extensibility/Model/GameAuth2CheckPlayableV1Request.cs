2a4d3f3ed6ade236c62b1d3a20ec7d64dc8c8a54
Darnell Williams <williams.darnell56@yahoo.com>
2021-12-23 01:55:23 -0500

    fix passport and authenticator auth processes

---
+++ HyddwnLauncher.Extensibility/Model/GameAuth2CheckPlayableV1Request.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace HyddwnLauncher.Extensibility.Model
+{
+    public struct GameAuth2CheckPlayableV1Request
+    {
+        /// <summary>
+        ///     The product id for the game being checked
+        /// </summary>
+        [JsonProperty("productId")]
+        public string ProductId { get; set; }
+    }
+}
